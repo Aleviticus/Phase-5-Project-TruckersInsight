484b31e31dbdaa3ea178ae110c5a5029f0fc117e
Aaron Levy <aaronlevypc21@gmail.com>
2024-03-13 11:06:56 -0400

    setup backend

---
+++ server/config.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_migrate import Migrate, session
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from model import  Truckers, Company, Load, db
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+    app.config['SQLALCEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = os.environ.get('SECRETKEY')
+    app.json.compact = False
+    
+    migrate = Migrate(app, db)
+    db.init_app(app)
+
+    CORS(app)
+
+    return app
